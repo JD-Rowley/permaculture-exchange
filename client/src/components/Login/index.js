da140ff28035a9da9f8fdf454f6974348bfd110b
JD Rowley <jdrowley1989@gmail.com>
2021-12-22 14:36:41 -0700

    added scripts

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -5,10 +5,12 @@ function Login() {
     return (
         <section>
             <form>
-                <label for="username"><b>Username:</b></label>
-                <input type="text" placeholder="Enter username" />
-                <label for="password"><b>Password:</b></label>
-                <input type="password" placeholder="Enter password" />
+                <div>
+                    <label for="username"><b>Username:</b></label>
+                    <input type="text" placeholder="Enter username" />
+                    <label for="password"><b>Password:</b></label>
+                    <input type="password" placeholder="Enter password" />
+                </div>
                 <button type="submit">Login</button>
             </form>
         </section>
