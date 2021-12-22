644ce664c9d4447cfbd7a4a8525e014b68d01838
JD Rowley <jdrowley1989@gmail.com>
2021-12-22 13:03:25 -0700

    added components folder

---
+++ client/src/components/About/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function About() {
+    return (
+        <section>
+            <h1>Utah Permaculture Collective</h1>
+        </section>
+    );
+}
+
+export default About;
