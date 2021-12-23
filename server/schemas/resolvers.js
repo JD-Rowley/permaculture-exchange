0b04d3fc0e876fc84f9042c749e9cab884acdadf
Andrew Durham <andrewdurham1094@gmail.com>
2021-12-23 16:03:21 -0700

    added addUser, login, and addPost mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -9,11 +9,13 @@ const resolvers = {
                 const userData = await User.findOne({ _id: context.user._id })
                     .select('-__v -password')
                     .populate('posts')
+                
+                return userData
             }
         },
         users: async () => {
             return User.find()
-                .select('-__v -password')
+                //.select('-__v -password')
                 .populate('posts')
         },
         user: async (parent, { username }) => {
@@ -27,6 +29,46 @@ const resolvers = {
         },
         post: async (parent, { _id }) => {
             return Post.findOne({ _id })
+        }
+    },
+    Mutation: {
+        addUser: async(parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user)
+
+            return { token, user }
+        },
+        login: async(parent, { email, password }) => {
+            const user = await User.findOne({ email })
+
+            if (!user) {
+                throw new AuthenticationError('Incorrect credentials')
+            }
+
+            const correctPw = await user.isCorrectPassword(password)
+
+            if(!correctPw) {
+                throw new AuthenticationError('Incorrect credentials')
+            }
+
+            const token = signToken(user)
+
+            return { token, user }
+        },
+        addPost: async(parent, args, context) => {
+            if (context.user) {
+                const post = await Post.create({ ...args, username: context.user.username });
+
+                await User.findByIdAndUpdate(
+                    { _id: context.user._id },
+                    { $push: { posts: post._id }},
+                    { new: true }
+                )
+
+                return post
+            }
+
+            throw new AuthenticationError('You need to be logged in!')
         }
     }
 };
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -20,12 +20,23 @@ const typeDefs = gql`
         username: String
     }
 
+    type Auth {
+        token: ID!
+        user: User
+    }
+
     type Query {
         me: User
         users: [User]
         user(username: String!): User
         posts(username: String): [Post]
         post(_id: ID!): Post
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        addPost(postTitle: String!, postDescription: String!): Post
     }
 `;
 
