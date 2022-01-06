4f4317c79c661c301a0e4a09fca0f75c8b32d24c
Tom Hertig <joehertig94@gmail.com>
2022-01-06 16:42:26 -0700

    added color and changes to trade page/comments page

--- client/src/components/CommentList/index.js
+++ client/src/components/CommentList/index.js
@@ -1,22 +1,25 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 function CommentList({ comments }) {
-    return (
-        <div className='comments-container'>
-            <h3>Comments:</h3>
-            {comments &&
-                comments.map(comment => (
-                    <div key={comment._id}>
-                        <p>{comment.commentText}</p>
-                        <Link to={`/profile/${comment.username}`}>
-                            {comment.username}
-                        </Link>
-                        <span> at {comment.createdAt}</span>
-                    </div>
-                ))}
-        </div>
-    );
-};
+  return (
+    <div className="comments-container">
+      <h3>COMMENTS:</h3>
+      {comments &&
+        comments.map((comment) => (
+          <div className="comment-response" key={comment._id}>
+            <p className="comment-paragraph">{comment.commentText}</p>
+            <Link
+              className="response-username"
+              to={`/profile/${comment.username}`}
+            >
+              {comment.username}
+            </Link>
+            <span> on {comment.createdAt}</span>
+          </div>
+        ))}
+    </div>
+  );
+}
 
 export default CommentList;
--- client/src/pages/Trade.js
+++ client/src/pages/Trade.js
@@ -1,31 +1,31 @@
-import React from 'react';
-import { useQuery } from '@apollo/client';
-import { QUERY_POSTS } from '../utils/queries';
-import TradeList from '../components/TradeList';
-import PostForm from '../components/PostForm';
-import Auth from '../utils/auth';
+import React from "react";
+import { useQuery } from "@apollo/client";
+import { QUERY_POSTS } from "../utils/queries";
+import TradeList from "../components/TradeList";
+import PostForm from "../components/PostForm";
+import Auth from "../utils/auth";
 
 function Trade() {
-    const { loading, data } = useQuery(QUERY_POSTS);
-    const posts = data?.posts || [];
-    const loggedIn = Auth.loggedIn();
+  const { loading, data } = useQuery(QUERY_POSTS);
+  const posts = data?.posts || [];
+  const loggedIn = Auth.loggedIn();
 
-    return (
-        <div className='container'>
-            {loggedIn && (
-                <div>
-                    <PostForm />
-                </div>
-            )}
-            <div className='card-container'>
-                {loading ? (
-                    <div>Loading...</div>
-                ) : (
-                    <TradeList posts={posts} title='This is the trade page!' />
-                )}
-            </div>
+  return (
+    <div className="container">
+      {loggedIn && (
+        <div>
+          <PostForm />
         </div>
-    );
+      )}
+      <div className="card-container">
+        {loading ? (
+          <div>Loading...</div>
+        ) : (
+          <TradeList posts={posts} title="THIS IS THE TRADE PAGE!" />
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Trade;
