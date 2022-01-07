f3b255d8c20639143b0720e86a70f74fdcca76fc
Andrew Durham <andrewdurham1094@gmail.com>
2022-01-06 19:56:43 -0700

    added delete function front end

--- client/src/components/TradeList/index.js
+++ client/src/components/TradeList/index.js
@@ -4,22 +4,34 @@ import { useMutation } from '@apollo/client';
 import { DELETE_POST } from '../../utils/mutations';
 
 const TradeList = ({ posts, title }) => {
+
+  function handleDelete(e) {
+
+    let removeId = e.target.id
+    let componentToDelete = document.getElementsByClassName(removeId)
+    componentToDelete = componentToDelete[0]
+    componentToDelete.remove()
+
+  }
+
     return (
       <div>
         <h3>{title}</h3>
         {posts &&
           posts.map(post => (
             <div key={post._id}>
-              <div className='card-header'>
-                <Link to={`/trade/${post._id}`}>
-                    <b>{post.postTitle}</b>
-                </Link>
-                    <p>posted by: <Link to={`/profile/${post.username}`}>{post.username}</Link> on {post.createdAt}</p>
+              <div className={`${post.postTitle}${post.postDescription}${post.createdAt}`}>
+                <div className={`card-header ${post.postTitle}`}>
+                  <Link to={`/trade/${post._id}`}>
+                      <b>{post.postTitle}</b>
+                  </Link>
+                      <p>posted by: <Link to={`/profile/${post.username}`}>{post.username}</Link> on {post.createdAt}</p>
+                </div>
+                <div className={`card-body ${post.postDescription}`}>
+                  <p>{post.postDescription}</p>
+                </div>
+                <button onClick={handleDelete} id={`${post.postTitle}${post.postDescription}${post.createdAt}`}>Delete</button> 
               </div>
-              <div className='card-body'>
-                <p>{post.postDescription}</p>
-              </div>
-              {/* <button>Delete</button> */}
             </div>
           ))}
       </div>
