ca6ff1f5ceef080e8a58741be5f4d54fbaf77c47
Andrew Durham <andrewdurham1094@gmail.com>
2022-01-06 17:51:44 -0700

    fixed error

--- client/src/components/PostForm/index.js
+++ client/src/components/PostForm/index.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
-import { useMutation } from "@apollo/client";
-import { ADD_POST } from "../../utils/mutations";
-import { QUERY_POSTS, QUERY_ME } from "../../utils/queries";
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { ADD_POST } from '../../utils/mutations';
+import { QUERY_POSTS, QUERY_ME } from '../../utils/queries';
 
 function PostForm() {
     const [formState, setFormState] = useState({ postTitle: '', postDescription: '' })
@@ -28,66 +28,58 @@ function PostForm() {
         }
     });
 
-      cache.writeQuery({
-        query: QUERY_ME,
-        data: { me: { ...me, posts: [...me.posts, addPost] } },
-      });
-    },
-  });
+    function handleChange(e) {
+        const { name, value } = e.target;
 
-  function handleChange(e) {
-    const { name, value } = e.target;
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    };
 
-    setFormState({
-      ...formState,
-      [name]: value,
-    });
-  }
+    async function handleFormSubmit(e) {
+        e.preventDefault();
 
-  async function handleFormSubmit(e) {
-    e.preventDefault();
+        try {
+            await addPost({
+                variables: { ...formState }
+            });
 
-    try {
-      await addPost({
-        variables: { ...formState },
-      });
+            setFormState({
+                postTitle: '',
+                postDescription: ''
+            });
+        } catch (e) {
+            console.error(e);
+        }
+    };
 
-      setFormState({
-        postTitle: "",
-        postDescription: "",
-      });
-    } catch (e) {
-      console.error(e);
-    }
-  }
-
-  return (
-    <div className="create-post">
-      <h3>CREATE A TRADE POST</h3>
-      {error && <div>Something went wrong...</div>}
-      <form className="post-form" onSubmit={handleFormSubmit}>
-        <input
-          type="text"
-          placeholder="What will you call this post?"
-          name="postTitle"
-          className="post-title"
-          id="title"
-          value={formState.postTitle}
-          onChange={handleChange}
-        />
-        <textarea
-          placeholder="Are you looking to trade?"
-          name="postDescription"
-          className="post-description"
-          value={formState.postDescription}
-          onChange={handleChange}
-        ></textarea>
-        <button type="submit" className="btn login-sign">
-          Submit
-        </button>
-      </form>
-    </div>
-  );
-}
+    return (
+        <div>
+            <h3>Create A Trade Post</h3>
+            {error && <div>Something went wrong...</div>}
+            <form className='post-form'
+                onSubmit={handleFormSubmit}>
+                <input 
+                    type='text' 
+                    placeholder='What will you call this post?'
+                    name='postTitle'
+                    className='post-title'
+                    id='title'
+                    value={formState.postTitle}
+                    onChange={handleChange}
+                />
+                <textarea 
+                    placeholder='Are you looking to trade?'
+                    name='postDescription'
+                    className='post-description'
+                    value={formState.postDescription}
+                    onChange={handleChange}
+                ></textarea>
+                <button type='submit' className='btn'>Submit</button>
+            </form>
+        </div>
+    );
+};
 
 export default PostForm;
